f8c24d5b20d024ca31eda027314f4e5d14bef9b4
mateo <mateomolfino09@gmail.com>
2022-11-27 13:54:29 -0300

    between

--- components/Markets.js
+++ components/Markets.js
@@ -6,7 +6,7 @@ const Markets = () => {
     <div>
        <div className='w-full relative overflow-hidden mt-20 mb-20 px-6 sm:px-12 lg:px-16' id='markets'>
             <Subtitle subtitle={'Markets we serve'}/>
-            <div className='flex flex-wrap flex-col md:flex-row w-full min-h-fit overflow-auto items-center justify-between mt-16'>
+            <div className='flex flex-wrap flex-col md:flex-row w-full min-h-fit overflow-auto items-center justify-between lg:justify-around mt-16'>
                 <div className='flex flex-wrap flex-col justify-center items-center mb-6'>
                 <img src="./market1.png" className='my-3 max-w-[136px] max-h-[136px] lg:max-h-[199px] lg:max-w-[199px]' alt="Foodstuff Icon" />
                 <h3 className='font-medium text-xs lg:text-2xl'> Foodstuff </h3>
