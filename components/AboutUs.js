18166e61ec5f429ca757361c4e1eae83de74f39e
mateo <mateomolfino09@gmail.com>
2022-12-16 12:04:39 -0300

    details

--- components/AboutUs.js
+++ components/AboutUs.js
@@ -9,13 +9,11 @@ const poppins = Poppins({
 
 const AboutUs = () => {
   return (     
-
-    
     <div>
         <div className='w-full relative overflow-hidden mt-20 pl-12 px-6 md:px-12 ' id='about'>
             <Subtitle subtitle={'About Us'}/>
             <div className='lg:pl-40'>
-              <p className='lg:font-light font-light text-xs lg:text-lg'>Garnet International has become a trusted partner with strong ties to the slaughtering and agriculture sectors. Our role has evolved as specialized commodity merchants with impressive connections, established relationships, and areas of expertise.</p>
+              <p className='lg:font-light font-light text-xs lg:text-lg'>    Garnet International has become a trusted partner with strong ties to the slaughtering and agriculture sectors. Our role has evolved as specialized merchants with established relationships worldwide.</p>
               <p className='lg:font-light font-light text-xs lg:text-lg mb-4'>Our involvement in the global marketplace has enabled us to identify emerging markets and anticipate market trends. These insights allow us to secure fair deals with suppliers and clients. </p>
               <p className='lg:font-light font-light text-xs lg:text-lg mb-4' >Our headquarters in the United States puts us in a scenario with sustained demand for renewable energy feedstock and other sectors. </p>
               <p className='lg:font-light font-light text-xs lg:text-lg mb-4'>Our work is based on these 4 principles:</p>
--- components/Markets.js
+++ components/Markets.js
@@ -3,8 +3,8 @@ import Subtitle from './Subtitle'
 
 const Markets = () => {
   return (
-    <div>
-       <div className='w-full relative overflow-hidden mt-20 mb-20 px-6 sm:px-12 lg:px-16' id='markets'>
+    <div id='markets'>
+       <div className='w-full relative overflow-hidden mt-20 mb-20 px-6 sm:px-12 lg:px-16' >
             <Subtitle subtitle={'Markets we serve'}/>
             <div className='flex flex-wrap flex-col md:flex-row w-full min-h-fit overflow-auto items-center justify-between lg:justify-around mt-16'>
                 <div className='flex flex-wrap flex-col justify-center items-center mb-6'>
@@ -12,13 +12,26 @@ const Markets = () => {
                 <h3 className='font-medium text-xs lg:text-2xl'> Foodstuff </h3>
                 </div>
                 <div className='flex flex-wrap flex-col justify-center items-center mb-6'>
+                <img src="./market3.png" className='my-3 max-w-[136px] max-h-[136px] lg:max-h-[199px] lg:max-w-[199px]' alt="Diesel Icon" />
+                <h3 className='font-medium text-xs lg:text-2xl'> Renewable Diesel</h3>
+                <h3 className='font-medium text-xs lg:text-2xl'>Feedstock</h3>
+
+                </div>
+                <div className='flex flex-wrap flex-col justify-center items-center mb-6'>
                 <img src="./market2.png" className='my-3 max-w-[136px] max-h-[136px] lg:max-h-[199px] lg:max-w-[199px]' alt="Animal Fed Icon" />
                 <h3  className='font-medium text-xs lg:text-2xl'> Animal Feed </h3>
                 </div>
                 <div className='flex flex-wrap flex-col justify-center items-center mb-6'>
-                <img src="./market3.png" className='my-3 max-w-[136px] max-h-[136px] lg:max-h-[199px] lg:max-w-[199px]' alt="Diesel Icon" />
-                <h3 className='font-medium text-xs lg:text-2xl'> Renewable Diesel </h3>
+                <img src="./market4.png" className='my-3 max-w-[136px] max-h-[136px] lg:max-h-[199px] lg:max-w-[199px]' alt="Animal Fed Icon" />
+                <h3  className='font-medium text-xs lg:text-2xl'> Oleochemicals </h3>
                 </div>
+                <div className='flex flex-wrap flex-col justify-center items-center mb-6'>
+                <img src="./market5.png" className='my-3 max-w-[136px] max-h-[136px] lg:max-h-[199px] lg:max-w-[199px]' alt="Animal Fed Icon" />
+                <h3  className='font-medium text-xs lg:text-2xl'> Soap  </h3>
+                <h3 className='font-medium text-xs lg:text-2xl'>Manufacturers</h3>
+
+                </div>
+
 
              </div>
         </div>
